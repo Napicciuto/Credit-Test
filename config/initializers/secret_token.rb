65409b73afbb6d54eb3c7afdc9c0b99f0766d976
napicciuto <nick@gmdx.com>
2013-09-10 20:20:39 -0400

    Setup Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CreditTest::Application.config.secret_token = 'f8626894c6c5f0ca2249c4d5edc258d7f48305ca4837313f209e450c668d88c6ce174dd92bc05eb8682abf96557886f7a41003219665fb4977bea9d4092f1730'
